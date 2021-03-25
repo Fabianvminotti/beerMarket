050eb91d0fdeb94d7af7833e392b5c69fb9c18a7
Fabianvminotti <fabianvminotti@gmail.com>
2021-03-25 02:16:30 -0300

    Menu armado

--- beer-market/src/componentes/Menu.js
+++ beer-market/src/componentes/Menu.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 
 const Menu = () => {
     return(
-        <div>
-            <h1>Menu</h1>
-        </div>
+        <nav className='navbar navbar-expand-lg navbar-light bg-warning ' >
+            <div className='container-fluid justify-content-around'>
+                <a className='navbar-brand'>
+                    <img src="https://i.ibb.co/TgcK0SZ/beerMark.png" height='35px' />
+                </a>
+                <form class="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Buscá tu birra" aria-label="Search"/>
+                    <button className="btn btn-outline-dark ml-2" type="submit">Buscar</button>
+                </form>
+                <button className='btn btn-danger'><FontAwesomeIcon icon={faUser} /></button>
+            </div>
+            <div className='row'>
+
+            </div>
+        </nav>
+        
     )
 }
 
